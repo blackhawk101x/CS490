35a3b77df06ca88a8139926dce9b8bbc062de6f6
Dakota Buell <dkb9@njit.edu>
2016-04-18 12:04:50 -0400

    Major cosmetic changes to the true/false view panes the remove feature now works for removing true/false questions from the database

--- Educator/trueFalse.php
+++ Educator/trueFalse.php
@@ -8,6 +8,15 @@ if($_SESSION['valid']!='teacher' || !isset($_SESSION['UCID'])){
 
 require_once 'curlHandle.php';
 
+function optActive($ans,$opt){
+	if(($ans=='T' && $opt) || ($ans=='F' && !$opt))
+		return "list-group-item-success";
+	else
+		return "list-group-item-danger";
+		
+}
+
+
 ?>
 
 <html>
@@ -15,6 +24,8 @@ require_once 'curlHandle.php';
 		<title>True or False</title>
 		<link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.min.css">
 		<script type="text/javascript" src="callHandler.js"></script>
+		<script type="text/javascript" src="rmQuest.js"></script>
+		<script type="text/javascript" src="trueFalse.js"></script>
 	</head>
 	<body style="padding-top: 70px;">
 		<?php require_once 'navBar.php';?>
@@ -71,15 +82,41 @@ require_once 'curlHandle.php';
 					<center>
 						<h3>Select an existing Question</h3>
 					</center>
-					<?php
-					
-					$questList=curlCall("https://web.njit.edu/~rs334/cs490/beta/rimi/test/get_meta.php",array("user_id"=>1,"question_type"=>1, "count"=>0));
-					//echo var_dump($questList);
-					foreach($questList as $key => $quest){
-						echo var_dump(get_object_vars($quest));
-						echo "<hr>";
-					}
-					?>
+					<div style="max-height:80%; overflow:scroll; overflow-x:hidden;">
+						<?php
+						
+						$questList=curlCall("https://web.njit.edu/~rs334/cs490/beta/rimi/test/get_meta.php",array("user_id"=>1,"question_type"=>1, "count"=>0));
+						//echo var_dump($questList);
+						foreach($questList as $key => $quest){
+							$quest= get_object_vars($quest);
+							?>
+							<div class="panel panel-default">
+								<div class="panel-body">
+									<center>
+										<h4>Question: <?php echo $quest['question']; ?></h4>
+										<div class="list-group">
+												<a class="list-group-item <?php echo optActive($quest['answer'],true); ?>">True</a>
+												<a class="list-group-item <?php echo optActive($quest['answer'],false); ?>">False</a>
+										</div>
+										<h5>Points: <?php echo $quest['points']; ?></h5>
+										
+										<button type="button" class="btn btn-primary btn-lg" aria-label="Left Align" disabled>
+											<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+											Edit
+										</button>
+										
+										<button type="button" class="btn btn-default btn-lg" aria-label="Left Align" onclick="rmQuest(<?php echo $quest['question_id']?>)">
+											<span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
+											Remove
+										</button>
+										
+									</center>
+								</div>
+							</div>
+							<?php
+						}
+						?>
+					</div>
 				</div>
 				
 				<div class="col-md-12">
@@ -90,6 +127,6 @@ require_once 'curlHandle.php';
 				</div>
 			</div>
 		</div>
-		<script type="text/javascript" src="trueFalse.js"></script>
+		
 	</body>
 </html>
