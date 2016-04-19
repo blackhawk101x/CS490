1b5de535f31982a43f3c3373617ea3cd16682d95
Dakota Buell <dkb9@njit.edu>
2016-04-18 23:51:00 -0400

    Ajusted the url path and curl data to middle ware specs

--- createTest.php
+++ createTest.php
@@ -4,13 +4,11 @@ $data = json_decode(file_get_contents('php://input'), true);
 require_once 'curlHandle.php';
 
 $data['user_id']=$_SESSION['id'];
-$ret = curlCall("https://web.njit.edu/~rs334/cs490/beta/rimi/exam/draft01.php ",$data);
+$ret = get_object_vars(curlCall("https://web.njit.edu/~rs334/cs490/beta/rimi/exam/exam.php",$data));
 
 
 $_SESSION['testId']=$ret['testId'];
 $_SESSION['testName']=$data['testName'];
 
-//echo json_encode(array('good'=>true));
-//echo json_encode($data_Curl);
 echo json_encode($ret);
 ?>
