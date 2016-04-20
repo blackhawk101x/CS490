3dc196b6bc83fcb1743f730797893e33083cc83a
Dakota Buell <dkb9@njit.edu>
2016-04-20 11:45:40 -0400

    Added the true/false pane to the Test Maker view made some formatting fixes to the mutichoice pane

--- Educator/testMaker.php
+++ Educator/testMaker.php
@@ -2,7 +2,7 @@
 session_start();
 
 
-if($_SESSION['valid']!='teacher' && !isset($_SESSION['UCID'])){
+if($_SESSION['valid']!='teacher' || !isset($_SESSION['UCID'])){
 	header('location: https://web.njit.edu/~dkb9/Software_Design_Project/');
 }
 
@@ -14,13 +14,13 @@ if(!isset($_SESSION['testId'])){
 require_once 'curlHandle.php';
 require_once 'navBar.php';
 
-$questList=curlCall("https://web.njit.edu/~rs334/cs490/beta/rimi/meta/exam/exam_info.php",array("user_id"=>$_SESSION['id'],"exam_id"=>"1"));
+$questList=curlCall("https://web.njit.edu/~rs334/cs490/beta/rimi/meta/exam/exam_info.php",array("user_id"=>$_SESSION['id'],"exam_id"=>$_SESSION['testId']));
 
 
 /*
 translates the the character into a reconizable string
 */
-function optTrans($ans){
+function mcTrans($ans){
 	switch($ans){
 		case 'A':
 			return 'option1';
@@ -36,14 +36,14 @@ function optTrans($ans){
 /*
 determines if the ais the current option
 */
-function optActive($ans,$option){
-	if(optTrans($ans)==$option)
+function mcActive($ans,$option){
+	if(mcTrans($ans)==$option)
 		return "list-group-item-success";
 	return "list-group-item-danger";
 } // end of function optActive
 
 /*
-
+builds the mutli chocie panel
 */
 function mcPanel($quest){
 	
@@ -52,21 +52,26 @@ function mcPanel($quest){
 		<h3>Question: <?php echo $quest['question']; ?></h3>
 		<div class="rows">
 			<div class="col-md-6">
-				<a class="list-group-item <?php echo optActive($quest['ans'],'option1'); ?>"><?php echo $quest['option1']; ?></a>
+				<div class="list-group">
+					<a class="list-group-item <?php echo mcActive($quest['ans'],'option1'); ?>"><?php echo $quest['option1']; ?></a>
+				</div>
 			</div>
 			<div class="col-md-6">
-				<a class="list-group-item <?php echo optActive($quest['ans'],'option2'); ?>"><?php echo $quest['option2']; ?></a>
+				<div class="list-group">
+					<a class="list-group-item <?php echo mcActive($quest['ans'],'option2'); ?>"><?php echo $quest['option2']; ?></a>
+				</div>
 			</div>
 			<div class="col-md-6">
-				<a class="list-group-item <?php echo optActive($quest['ans'],'option3'); ?>"><?php echo $quest['option3']; ?></a>
+				<div class="list-group">
+					<a class="list-group-item <?php echo mcActive($quest['ans'],'option3'); ?>"><?php echo $quest['option3']; ?></a>
+				</div>
 			</div>
 			<div class="col-md-6">
-				<a class="list-group-item <?php echo optActive($quest['ans'],'option4'); ?>"><?php echo $quest['option4']; ?></a>
+				<div class="list-group">
+					<a class="list-group-item <?php echo mcActive($quest['ans'],'option4'); ?>"><?php echo $quest['option4']; ?></a>
+				</div>
 			</div>
-			<hr>
-			<br>
-			<hr>
-			<br>			
+						
 			
 			<button type="button" class="btn btn-primary btn-lg" aria-label="Left Align" disabled>
 				<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
@@ -83,6 +88,51 @@ function mcPanel($quest){
 	
 }
 
+/*
+detrimines if the current option is the correct one.
+If it is, it sets the look of the item to green, red otherwise
+*/
+function tfActive($ans,$opt){
+	if(($ans=='T' && $opt) || ($ans=='F' && !$opt))
+		return "list-group-item-success";
+	else
+		return "list-group-item-danger";
+		
+} // end of function 'tfActive'
+
+/*
+builds the panel for the true and false question
+*/
+function tfPanel($quest){
+	?>
+	<center>
+		<h3>Question: <?php echo $quest['question'];?><h3>
+		<div class="rows">
+			<div class="col-md-6">
+				<div class="list-group">
+					<a class="list-group-item <?php echo tfActive($quest['ans'],true); ?>">True</a>
+				</div>
+			</div>
+			
+			<div class="col-md-6">
+				<div class="list-group">
+					<a class="list-group-item <?php echo tfActive($quest['ans'],false); ?>">False</a>
+				</div>
+			</div>
+					
+			
+			<button type="button" class="btn btn-primary btn-lg" aria-label="Left Align" disabled>
+				<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+				Edit Question
+			</button>
+			<button type="button" class="btn btn-default btn-lg" aria-label="Left Align" onclick="rmQuestTest(<?php echo $quest['test_q_id']; ?>)">
+				<span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
+				Remove from Test
+			</button>
+		</div>
+	</center>
+	<?php
+} // end of function 'tfPanel'
 
 
 ?>
@@ -99,6 +149,7 @@ function mcPanel($quest){
 		<div class="jumbotron">
 	   		<div class="container">
         		<h1><?php echo $_SESSION['testName'];?></h1>
+				
     		</div>
     	</div>
     	
@@ -116,6 +167,9 @@ function mcPanel($quest){
 							echo var_dump($quest);
 							if($quest['type_key']=='1'){
 								mcPanel($quest);
+							}
+							elseif($quest['type_key']=='2'){
+								tfPanel($quest);
 							}
 							?>
 						</div>
