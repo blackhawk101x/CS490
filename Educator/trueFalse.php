ceb9f93cae879e78353b27725b026bce43948f42
Dakota Buell <dkb9@njit.edu>
2016-04-20 15:12:06 -0400

    The add to exam button is toggled for any questions that are already in the database

--- Educator/trueFalse.php
+++ Educator/trueFalse.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-require_once 'navBar.php';
 
 //proper authentication
 if($_SESSION['valid']!='teacher' || !isset($_SESSION['UCID'])){
@@ -8,6 +7,44 @@ if($_SESSION['valid']!='teacher' || !isset($_SESSION['UCID'])){
 }
 
 require_once 'curlHandle.php';
+require_once 'navBar.php';
+
+
+/*
+If the user is editing a test, this will get all the multichoice question from the db that are already part of this test
+*/
+$questDB=array();
+if(isset($_SESSION['testId'])){
+	$currExam=curlCall("https://web.njit.edu/~rs334/cs490/beta/rimi/meta/exam/exam_info.php",array("user_id"=>$_SESSION['id'],"exam_id"=>$_SESSION['testId']));
+	foreach($currExam as $key=>$quest){
+		$quest=get_object_vars($quest);
+		if($quest['type_key']=='2')
+			$questDB[$key]=$quest['quest_id'];
+	}
+}
+
+/* 
+Creates the add to test button according to whether it is already in the exam or the user is even edditing the exam
+*/
+function addToExamBtn($questId,$questDB){
+	if(isset($_SESSION['testId'])){
+		?> <button type="button" class="btn btn-primary btn-lg" aria-label="Left Align" <?php 
+		if(in_array($questId,$questDB)){
+			?> disabled>
+			Already in Test
+			<?php
+		}
+		else{
+			?> onclick="addQuestTest(<?php echo $questId; ?>)"> 
+			<span class="glyphicon glyphicon-log-in" aria-hidden="true"></span>
+				Add to Test
+			
+			<?php
+		}
+		?> </button><?php
+	}
+	// do nothing for now
+} // end of function addtoExamBtn
 
 function optActive($ans,$opt){
 	if(($ans=='T' && $opt) || ($ans=='F' && !$opt))
@@ -38,6 +75,7 @@ function optActive($ans,$opt){
 	<body style="padding-top: 70px;">
 		<?php navBar(); ?>
 		<div class="container">
+		<?php toolBar();?>
 			<div class="rows">
 				<div class="col-md-6">
 					<form id="trueFalseForm">
@@ -108,15 +146,8 @@ function optActive($ans,$opt){
 										</div>
 										<h5>Points: <?php echo $quest['points']; ?></h5>
 										
-										<?php 
-										if(isset($_SESSION['testId'])){
-											?>
-											<button type="button" class="btn btn-primary btn-lg" aria-label="Left Align" onclick="addQuestTest(<?php echo $quest['question_id']; ?>)">
-												<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
-												Add to Test
-											</button>
-											<?php
-										}
+										<?php
+											addtoExamBtn($quest['question_id'],$questDB);
 										?>
 										
 										<button type="button" class="btn btn-default btn-lg" aria-label="Left Align" onclick="rmQuest(<?php echo $quest['question_id']?>)">
