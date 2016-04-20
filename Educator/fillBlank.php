89337e91096153eb8756233c2a03e4bbbf759725
Dakota Buell <dkb9@njit.edu>
2016-04-20 15:20:30 -0400

    The fill-in-the-blank question bank toggles the button if it is already in the database

--- Educator/fillBlank.php
+++ Educator/fillBlank.php
@@ -11,7 +11,41 @@ require_once 'curlHandle.php';
 require_once 'navBar.php';
 
 
+/*
+If the user is editing a test, this will get all the multichoice question from the db that are already part of this test
+*/
+$questDB=array();
+if(isset($_SESSION['testId'])){
+	$currExam=curlCall("https://web.njit.edu/~rs334/cs490/beta/rimi/meta/exam/exam_info.php",array("user_id"=>$_SESSION['id'],"exam_id"=>$_SESSION['testId']));
+	foreach($currExam as $key=>$quest){
+		$quest=get_object_vars($quest);
+		if($quest['type_key']=='3')
+			$questDB[$key]=$quest['quest_id'];
+	}
+}
 
+/* 
+Creates the add to test button according to whether it is already in the exam or the user is even edditing the exam
+*/
+function addToExamBtn($questId,$questDB){
+	if(isset($_SESSION['testId'])){
+		?> <button type="button" class="btn btn-primary btn-lg" aria-label="Left Align" <?php 
+		if(in_array($questId,$questDB)){
+			?> disabled>
+			Already in Test
+			<?php
+		}
+		else{
+			?> onclick="addQuestTest(<?php echo $questId; ?>)"> 
+			<span class="glyphicon glyphicon-log-in" aria-hidden="true"></span>
+				Add to Test
+			
+			<?php
+		}
+		?> </button><?php
+	}
+	// do nothing for now
+} // end of function addtoExamBtn
 
 
 
@@ -88,14 +122,7 @@ require_once 'navBar.php';
 											<h4>Answer: <?php echo $quest['ans'];?></h4>
 											
 											<?php 
-											if(isset($_SESSION['testId'])){
-												?>
-												<button type="button" class="btn btn-primary btn-lg" aria-label="Left Align" onclick="addQuestTest(<?php echo $quest['question_id']; ?>)">
-													<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
-													Add to Test
-												</button>
-												<?php
-											}
+												addtoExamBtn($quest['question_id'],$questDB);
 											?>
 											
 											<button type="button" class="btn btn-default btn-lg" aria-label="Left Align" onclick="rmQuest(<?php echo $quest['question_id']?>)">
