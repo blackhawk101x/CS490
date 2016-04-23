fec65051723d4708a7c3443ac5ef7e72f07c7d6b
Dakota Buell <dkb9@njit.edu>
2016-04-23 16:35:29 -0400

    Updated the addquest to also add the quest to the test if the test id is set

--- Educator/addQuest.php
+++ Educator/addQuest.php
@@ -8,18 +8,25 @@ $data['user_id']=$_SESSION['id'];
 if($data['type']=='mc'){
 	//$data['question_type']='0';
 	$ret=get_object_vars(curlCall('https://web.njit.edu/~rs334/cs490/beta/rimi/questions/mc/middle_mc.php',$data));
-	echo json_encode(array('valid'=>$ret['valid']));
+	//echo json_encode(array('valid'=>$ret['valid']));
 }
 elseif($data['type']=='tf'){
 	$ret=get_object_vars(curlCall('https://web.njit.edu/~rs334/cs490/beta/rimi/questions/tf/middle_tf.php',$data));
-	echo json_encode(array('valid'=>$ret['valid']));
+	//echo json_encode(array('valid'=>$ret['valid']));
 }
 elseif($data['type']=='fb'){
 	$ret=get_object_vars(curlCall('https://web.njit.edu/~rs334/cs490/beta/rimi/questions/fb/middle_fb.php',$data));
-	echo json_encode(array('valid'=>$ret['valid']));
+	//echo json_encode(array('valid'=>$ret['valid']));
 }
 else{
-	echo json_encode(array('valid'=>false));
+	//echo json_encode(array('valid'=>false));
+	$ret=array('valid'=>false);
 }
+
+if(isset($_SESSION['testId']) && $ret['valid'] ){
+	$ret= get_object_vars(curlCall("https://web.njit.edu/~rs334/cs490/beta/rimi/exam/add_exam_ques.php",array('quest_id'=>$ret['ques_id'],'user_id'=>$_SESSION['id'], 'exam_id'=>$_SESSION['testId'])));
 	
+}
+
+echo json_encode($ret);
 ?>
