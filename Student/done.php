6c027a6e9dcfe114c47b7ac7d18fbcfd01478c91
Dakota Buell <dkb9@njit.edu>
2016-04-24 18:29:11 -0400

    Handles the submittion of the student's answers

---
+++ Student/done.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+$data = json_decode(file_get_contents('php://input'), true);
+require_once 'curlHandle.php';
+
+$data= array('answers'=>$_SESSION['ans']);
+$data['user_id']=$_SESSION['id'];
+$data['test_id']=$_SESSION['testId'];
+
+$ret=curlCall("https://web.njit.edu/~rs334/cs490/beta/rimi/student/answers/get_answer3.php",$data);
+
+//echo json_encode(array('type'=>gettype($ret)));
+
+echo json_encode($ret);
+
+
+$ret=get_object_vars($ret);
+
+//echo json_encode($data);
+//echo json_encode(array('type'=>gettype($data['answers'])));
+
+
+//echo json_encode($ret);
+//echo json_encode(array('valid'=>true));
+?>
