2af8c47543e449e1d131abbce4e149e084fcbb51
Dakota Buell <dkb9@njit.edu>
2016-04-24 16:47:31 -0400

    updated the test taking views

--- Student/views.php
+++ Student/views.php
@@ -16,25 +16,29 @@ function mcView($quest){
 					</div>
 					<h2><?php echo $quest['question']; ?></h2>
 					<hr>
-					<button type="button" class="btn btn-primary btn-lg" aria-label="Left Align"  id="option1">
-						<?php echo $quest['option1']; ?>
-					</button>
-					
-					<button type="button" class="btn btn-danger btn-lg" aria-label="Left Align"  id="option2">
-						<?php echo $quest['option2']; ?>
-					</button>
-					
-					<button type="button" class="btn btn-success btn-lg" aria-label="Left Align"  id="option3">
-						<?php echo $quest['option3']; ?>
-					</button>
-					
-					<button type="button" class="btn btn-warning btn-lg" aria-label="Left Align"  id="option4">
-						<?php echo $quest['option4']; ?>
-					</button>
-					
-					
-					
-					
+					<div class="row">
+						<div class="col-md-3">
+							<button type="button" class="btn btn-primary btn-lg btn-block" aria-label="Left Align"  id="option1">
+								<?php echo $quest['option1']; ?>
+							</button>
+						</div>
+						<div class="col-md-3">
+							<button type="button" class="btn btn-danger btn-lg btn-block" aria-label="Left Align"  id="option2">
+								<?php echo $quest['option2']; ?>
+							</button>
+						</div>
+						<div class="col-md-3">
+							<button type="button" class="btn btn-success btn-lg btn-block" aria-label="Left Align"  id="option3">
+								<?php echo $quest['option3']; ?>
+							</button>
+						</div>
+						<div class="col-md-3">
+							<button type="button" class="btn btn-warning btn-lg btn-block" aria-label="Left Align"  id="option4">
+								<?php echo $quest['option4']; ?>
+							</button>
+						</div>
+						
+					</div>
 				</center>
 			</div>
 		</div>
@@ -53,25 +57,24 @@ function tfView($quest){
 	<form id="tfView">
 		<div class="panel panel-default">
 			<div class="panel-body">
-			
-				<div class="alert alert-danger" style="display:none" id="errorMsg">
-					<strong>Error: </strong> Please select True or False
-				</div>
-				
 				<h3>True or False</h3>
+				
 				<center>
+					<div class="alert alert-danger" style="display:none" id="errorMsg">
+						<strong>Error: </strong> Please select True or False
+					</div>
 					<h2><?php echo $quest['question']; ?></h2>
 					<hr>
 					<div class="row">
 						<div class="col-md-3">
 						</div>
 						<div class="col-md-3">
-							<button type="button" class="btn btn-primary btn-lg btn-block" aria-label="Left Align"  id="true" name="tf">
+							<button type="button" class="btn btn-success btn-lg btn-block" aria-label="Left Align"  id="true" name="tf">
 								True
 							</button>
 						</div>
 						<div class="col-md-3">
-							<button type="button" class="btn btn-default btn-lg btn-block" aria-label="Left Align"  id="false" name="tf">
+							<button type="button" class="btn btn-danger btn-lg btn-block" aria-label="Left Align"  id="false" name="tf">
 								False
 							</button>
 						</div>
@@ -106,9 +109,17 @@ function fbView($quest){
 				<center>
 					<h2><?php echo $quest['question']; ?></h2>
 					<hr>
-					<div class="form-group">
-						<label for="optionA">Answer</label>
-						<input id="answer" type="text" class="form-control" placeholder="Answer" required>
+					<div class="row">
+						<div class="col-md-4">
+						</div>
+						<div class="col-md-4">
+							<div class="form-group">
+								<input id="answer" type="text" class="form-control" placeholder="Answer" required>
+							</div>
+						</div>
+						<div class="col-md-4">
+						</div>
+						
 					</div>
 				</center>
 			</div>
